4570d3b3f0e3e778281515fba43fdb84eade00f7
Jakub Sobel <jakub.sobel@yougov.com>
2022-08-08 13:14:13 +0200

    Test tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,16 @@
   import Form from "./components/form/Form.svelte";
 </script>
 
-<Form />
+<main
+  class="min-h-screen	flex flex-col items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500"
+>
+  <div class="w-1/2 m-4">
+    <Form />
+  </div>
+</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
--- src/components/form/Form.svelte
+++ src/components/form/Form.svelte
@@ -1,4 +1,4 @@
-<script type="text/typescript">
+<script lang="ts">
   import { form, field } from "svelte-forms";
   import {
     required,
@@ -12,56 +12,64 @@
   const myForm = form(name, email, phone);
 </script>
 
-<div class="form-section">
-  <h1>This is a form title</h1>
-  <p class="description">This is a form description</p>
+<div
+  class="p-8 shadow-md rounded-lg bg-slate-300 backdrop-blur-sm bg-opacity-75 flex flex-col justify-center"
+>
+  <h1 class="text-2xl text-center text-slate-800">This is a form title</h1>
+  <p class="text-lg text-center text-slate-800">This is a form description</p>
 
-  <section class="content">
-    <label for="name">Name</label>
-    <input type="text" name="name" bind:value={$name.value} />
-    {#if $myForm.hasError("name.required")}
-      <div class="error">Name is required</div>
-    {/if}
-    <label for="email">E-mail</label>
-    <input type="text" name="email" bind:value={$email.value} />
-    {#if $myForm.hasError("email.required")}
-      <div class="error">E-mail is required</div>
-    {/if}
-    {#if $myForm.hasError("email.not_an_email")}
-      <div class="error">Provide correct email</div>
-    {/if}
-    <label for="phone">Phone</label>
-    <input type="text" name="phone" bind:value={$phone.value} />
-    {#if $myForm.hasError("phone.required")}
-      <div class="error">Phone is required</div>
-    {/if}
-    {#if $myForm.hasError("phone.pattern")}
-      <div class="error">Provide correct phone</div>
-    {/if}
-    <button on:click={myForm.validate}>Submit</button>
+  <section class="flex flex-col justify-center">
+    <label class="mb-2" for="name">Name</label>
+    <input
+      class="p-2 rounded-lg"
+      type="text"
+      name="name"
+      bind:value={$name.value}
+    />
+    <div class="mb-6">
+      {#if $myForm.hasError("name.required")}
+        <div class="text-xs text-red-500">Name is required</div>
+      {/if}
+    </div>
+    <label class="mb-2" for="email">E-mail</label>
+    <input
+      class="p-2 rounded-lg"
+      type="text"
+      name="email"
+      bind:value={$email.value}
+    />
+    <div class="mb-6">
+      {#if $myForm.hasError("email.required")}
+        <div class="text-xs text-red-500">E-mail is required</div>
+      {/if}
+      {#if $myForm.hasError("email.not_an_email")}
+        <div class="text-xs text-red-500">Provide correct email</div>
+      {/if}
+    </div>
+    <label class="mb-2" for="phone">Phone</label>
+    <input
+      class="p-2 rounded-lg"
+      type="text"
+      name="phone"
+      bind:value={$phone.value}
+    />
+    <div class="mb-6">
+      {#if $myForm.hasError("phone.required")}
+        <div class="text-xs text-red-500">Phone is required</div>
+      {/if}
+      {#if $myForm.hasError("phone.pattern")}
+        <div class="text-xs text-red-500">Provide correct phone</div>
+      {/if}
+    </div>
+    <button
+      on:click={myForm.validate}
+      class="bg-violet-400 rounded-lg shadow-md p-2">Submit</button
+    >
   </section>
 
-  <p>
-    {JSON.stringify($myForm, null, 2)}
-  </p>
+  <pre class="bg-white p-4 mt-8 rounded-lg bg-opacity-75">{JSON.stringify(
+      $myForm,
+      null,
+      2
+    )}</pre>
 </div>
-
-<style type="text/scss">
-  $red: red;
-  $green: green;
-  $blue: blue;
-
-  .form-section {
-    h1 {
-      color: $green;
-    }
-
-    .description {
-      color: $blue;
-    }
-
-    .error {
-      color: $red;
-    }
-  }
-</style>
